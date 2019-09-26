ce9f20bbb7ad83284e42628d51ade14d481fba6d
trilamsr <tree@Tree.local>
2019-09-25 17:43:54 -0700

    merging set-up

--- tree_money/urls.py
+++ tree_money/urls.py
@@ -17,5 +17,5 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('apps/main/urls')),
+    url(r'^', include('apps.main.urls')),
 ]
