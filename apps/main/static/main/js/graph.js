222e65557db63739964e883ac7c14ec4744c0265
logandee <logandeehenderson@gmail.com>
2019-09-27 08:28:34 -0700

    working graph with ajax

--- apps/main/static/main/js/graph.js
+++ apps/main/static/main/js/graph.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-            $(document).on('submit', '#search_ticker', function (e) {
+            $(document).on('submit', '#ticker_form', function (e) {
                 e.preventDefault();
                 $.ajax({
                     url: '/getTicker',
@@ -7,57 +7,7 @@ $(document).ready(function () {
                     data: $(this).serialize(),
                     success: function (serverResponse) {
                         $('.graph').html(serverResponse);
-                        async function getData() {
-
-                    const response = await fetch(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-chart?interval=5m&region=US&symbol=${ticker}&lang=en&range=${graph_range}`, {
-                        "method": "GET",
-                        "headers": {
-                            "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-                            "x-rapidapi-key": "9cf6f84669mshf37812fae4d0b86p19dc92jsnac00aa2c3e97"
-                        }
-                    })
-                    const myJson = await response.json();
-                    console.log(JSON.stringify(myJson));
-                    var ptag = document.getElementById('data')
-
-                    var chartData = []
-                    var listOfOpen = myJson.chart.result[0].indicators.quote[0].open
-                    console.log(myJson.chart.result)
-                    for (var i = 0; i < listOfOpen.length; i++) {
-                        chartData.push(listOfOpen[i])
                     }
-                    console.log(chartData)
-                    // chart data
-
-                    var ctx = document.getElementById('myChart').getContext('2d');
-
-                    var myChart = new Chart(ctx, {
-                        type: 'line',
-                        data: {
-                            labels: chartData,
-                            datasets: [{
-                                label: 'Apple',
-                                fill: false,
-                                data: chartData,
-                                borderColor: 'rgba(44, 130, 201, 1)',
-                                borderWidth: 1
-                            }, ]
-                        },
-                        options: {
-                            scales: {
-                                yAxes: [{
-                                    ticks: {
-                                        beginAtZero: false
-                                    }
-                                }]
-                            }
-                        }
-                    });
-
-                }
-                getData()
-
-            }
-        })
-    })
+                })
+            })
 })
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
-import request
+from django.shortcuts import render, redirect, HttpResponse
+import requests
+import json
+import time
 
 # Create your views here.
 
@@ -15,11 +17,11 @@ def get_stats(req):
 
 
 
-def getChartData(req):
-    ticker = req.POST['ticker']
-    graph_range = req.POST['graph_range']
+def getChartData(ticker, graph_range, interval='5m'):
+    ticker = ticker
+    graph_range = graph_range
     url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-chart"
-    querystring = {f"interval":"5m","region":"US","symbol":"{ticker}","lang":"en","range":"{graph_range}"}
+    querystring = {"interval":interval,"region":"US","symbol":ticker,"lang":"en","range":graph_range}
     headers = {
     'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
     'x-rapidapi-key': "9cf6f84669mshf37812fae4d0b86p19dc92jsnac00aa2c3e97"
@@ -33,5 +35,19 @@ def getChartData(req):
 #     return redirect(f"/getGraph/{ticker}/{graph_range}")
 
 def getGraph(req):
-    response = getChartData(req.POST['ticker'].upper()).json()
-    return render(req, 'main/graph.html', response)
+    response = getChartData(req.POST['ticker'].upper(),req.POST['graph_range'],req.POST['interval']).json()
+    print(type(response))
+    gettime = response['chart']['result'][0]['timestamp']
+    # print(gettime)
+    # for i in range(len(gettime)):
+    #     gettime[i] = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(gettime[i]))
+    #     print(gettime[i])
+    # print(gettime)
+    context = {
+        'symbol': str(req.POST['ticker'].upper()),
+        'time': response['chart']['result'][0]['timestamp'],
+        'data': response['chart']['result'][0]['indicators']['quote'][0]['open']
+    }
+    return render(req, 'main/graph.html', context)
+
+    # return HttpResponse(response)
