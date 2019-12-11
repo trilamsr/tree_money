c4332017eae1692cf07269002576d2bd243b1006
trilamsr <trilam153@gmail.com>
2019-12-11 02:14:50 -0800

    final version

--- apps/main/static/main/js/controller.js
+++ apps/main/static/main/js/controller.js
@@ -1,7 +1,6 @@
 function getServerResponse(str) {
-    $(document).on('click', `#${str}`, function(e){
+    $(document).on('click', `#${str}`, function (e) {
         e.preventDefault();
-        // console.log(str)
         $.ajax({
             url: `/get_${str}`,
             method: 'post',
@@ -10,27 +9,32 @@ function getServerResponse(str) {
                 csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
             },
             success: function (serverResponse) {
-                console.log(serverResponse)
                 $('#stats').html(serverResponse)
+                $('#graph').html('')
             },
             error: function (serverResponse) {
-                console.log(serverResponse)
+                $('#graph').html('')
                 $('#stats').html(`<h1>Shit happened in ${str}</h1>`)
             },
         })
     })
+    // $('#graph').html();
 }
 
 function setPage() {
     $.ajax({
         url: '/get_nav',
         method: 'post',
-        data: {ticker: $('#search_ticker').val(),
-            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()},
-        success: function (serverResponse) {$('#navigation_bar').html(serverResponse);},
+        data: {
+            ticker: $('#search_ticker').val(),
+            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+        },
+        success: function (serverResponse) {
+            $('#navigation_bar').html(serverResponse);
+        },
     })
     let IDs = ['technical', 'profile', 'financial', 'news']
-    for (let id of IDs){
+    for (let id of IDs) {
         getServerResponse(id)
     }
     (() => {
@@ -51,4 +55,40 @@ function setPage() {
 $('#search_submit').click(function (e) {
     e.preventDefault();
     setPage();
+})
+
+function getInterval(str) {
+    if (str === '1d') {
+        return '5m'
+    }
+    if (str === '5d') {
+        return '60m'
+    } else {
+        return '1d'
+    }
+}
+
+$(document).on('change', "input[name='graph_range']", function (e) {
+    e.preventDefault();
+    let val =$("input[name='graph_range']").val()
+    console.log(val)
+    console.log(getInterval(val))
+    $.ajax({
+        url: '/getTicker1',
+        method: 'post',
+        data: {
+            graph_range: val,
+            ticker: $("input[name='ticker']").val(),
+            interval: getInterval(val),
+            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+        },
+        success: function (serverResponse) {
+            console.log(serverResponse)
+            $('#graph').html(serverResponse);
+        },
+        error: function (re) {
+            console.log(serverResponse)
+            $('#graph').html('<h1>Graph is not happy</h1>');
+        }
+    })
 })
